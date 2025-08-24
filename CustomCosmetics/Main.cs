262a27ba7717ed18e6bdee6cb182ff1c1d1634ad
mxyx0412 <QZH0412@126.com>
2025-08-24 18:52:19 +0800

    Fix

--- CustomCosmetics/Main.cs
+++ CustomCosmetics/Main.cs
@@ -1,12 +1,13 @@
+global using HarmonyLib;
 global using static CustomCosmetics.Helpers;
 global using static CustomCosmetics.Logger;
-global using HarmonyLib;
+global using ISystem = Il2CppSystem.Collections.Generic;
 global using Main = CustomCosmetics.CosmeticsManager;
-using System.IO;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Unity.IL2CPP;
 using CustomCosmetics.CustomHats;
+using System.IO;
 
 namespace CustomCosmetics;
 
@@ -24,28 +25,25 @@ public partial class CosmeticsManager : BasePlugin
     public Harmony Harmony { get; } = new(Id);
 
     internal static string RepositoryUrl => Repository.Value.GithubUrl();
+    public static ConfigEntry<bool> CosmeticsUnlocker { get; set; }
     public static ConfigEntry<string> Repository { get; set; }
     public static ConfigEntry<bool> LocalHats { get; set; }
-    public static ConfigEntry<bool> CosmeticsUnlocker { get; set; }
 
     public override void Load()
     {
         SetLogSource(Log);
         Harmony.PatchAll();
 
-        CosmeticsUnlocker = Config.Bind("General", "Cosmetics Unlocker", false, "Unlock all cosmetics in the game, including paid ones.");
+        CosmeticsUnlocker = Config.Bind("General",
+            "Cosmetics Unlocker", false, "Unlock all cosmetics in the game, including paid ones.");
 
         LocalHats = Config.Bind("CustomHats", "Local Hats", false,
             "Enable to only use local hat files without downloading from online repository");
-        Repository = Config.Bind("CustomHats", "Repository Source", "https://raw.githubusercontent.com/TheOtherRolesAU/TheOtherHats/master"
-            , "URL for downloading custom hats when Local Hats is disabled");
-
-
-
+        Repository = Config.Bind("CustomHats", "Repository Source", "https://raw.githubusercontent.com/TheOtherRolesAU/TheOtherHats/master",
+            "URL for downloading custom hats when Local Hats is disabled");
 
         Instance = this;
         CustomHatManager.LoadHats();
         Message("CosmeticsManager Loading!");
     }
-
 }
